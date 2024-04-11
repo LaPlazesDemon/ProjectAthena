9cb5381f65fb070b466b170ac304507d24fb2658
LaPlazesDemon <164425143+LaPlazesDemon@users.noreply.github.com>
2024-04-10 20:14:50 -0400

    Added txt.js

---
+++ formatters/txt.js
@@ -0,0 +1,12 @@
+const formatterName = "Text File"
+const formatterId = "txt"
+
+const format = function(storyData) {
+
+}
+
+module.exports = {
+    formatterName: formatterName,
+    formatterId: formatterId,
+    format: format
+}
