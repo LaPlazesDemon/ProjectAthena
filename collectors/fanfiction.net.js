c7b06fb2db07d197325d8c7af3e4006f3f07a83a
LaPlazesDemon <164425143+LaPlazesDemon@users.noreply.github.com>
2024-04-10 20:14:32 -0400

    under dev

---
+++ collectors/fanfiction.net.js
@@ -0,0 +1,12 @@
+var collectorName = "Fanfiction.net";
+var collectorRegex = /^(?:https?:\/\/)?(?:www\.)?fanfiction\.net\/s\/\d+\/\d+\/?.*$/i;
+
+var collectionProcess = function(url) {
+
+}
+
+module.exports = {
+    collectorName: collectorName,
+    collectorRegex, collectorRegex,
+    process: collectionProcess
+}
