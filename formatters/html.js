4dbdbde7edaaaf31509e8979db3bde637a76f247
LaPlazesDemon <164425143+LaPlazesDemon@users.noreply.github.com>
2024-04-16 23:15:59 -0400

    Added support for generating Table of Contents

--- formatters/html.js
+++ formatters/html.js
@@ -5,20 +5,21 @@ const cheerio = require("cheerio");
 const config = require('../bin/config.json');
 const fs = require('fs');
 
-const format = function(storyData) {
+const format = function(storyData, options) {
 
     var output = `
 <h1>${storyData.title}</h1>
 <h3>By: ${storyData.author}</h3>
-${(function(){if (storyData.tags != undefined) {return `Tags: ${storyData.tags.join(", ")}<br>`} else {return ""}})()}
+${(function(){if (storyData.tags != undefined) {return `<div class='tags'><strong>Tags:</strong> ${storyData.tags.join(", ")}</div>`} else {return ""}})()}
+${(function(){if (!options.skipToc) {return generateTOC(storyData);} else {return ""}})()}
 ${(function(){if (storyData.introduction != undefined) {return `<h3>Introduction</h3><p>${storyData.introduction}</p>`} else {return ""}})()}
 `;
 
-storyData.chapter_html.forEach(chapter => {
+storyData.chapter_html.forEach((chapter, index) => {
 
     output += `
     
-    <h3>${chapter.title.trim()}</h3>`;
+    <h3 id="chapter${index}">${chapter.title.trim()}</h3>`;
 
     text = chapter.data;
     if (text.replaceAll(" ",).replaceAll("\n", "") !== "") {
@@ -27,12 +28,21 @@ storyData.chapter_html.forEach(chapter => {
 
 });
 
-var htmlTemplate = fs.readFileSync("./bin/html_template.html")
-var $ = cheerio.load(htmlTemplate);
+    var htmlTemplate = fs.readFileSync("./bin/html_template.html")
+    var $ = cheerio.load(htmlTemplate);
 
-$('body').html(output);
-fs.writeFileSync(`${config.output.directory}/${storyData.title} by ${storyData.author}.html`, $.html());
+    $('body').html(output);
+    fs.writeFileSync(`${config.output.directory}/${storyData.title} by ${storyData.author}.html`, $.html());
 }
+
+function generateTOC(data) {
+    var toc = "<div class='toc'><h3>Table of Contents</h3>";
+    toc += data.chapter_html.map((chapter, index) => `- <a href='#chapter${index}'>${chapter.title.trim()}</a>\n`).join('<br>');
+    toc += "</div>"
+
+    return toc;
+}
+
 
 module.exports = {
     formatterName: formatterName,
--- run.js
+++ run.js
@@ -21,32 +21,34 @@ fs.readdirSync("formatters").forEach(file => {
 
 // Define Command Line Arguments
 const options = yargs
-  .usage('Usage: $0 [options]')
-  .option('url', {
+.usage('Usage: $0 [options]')
+.option('url', {
     describe: 'URL of the work',
     type: 'string',
     demandOption: true 
-  })
-  .option('format', {
+})
+.option('format', {
     describe: 'Which formatter to use',
     type: 'string',
     demandOption: true,
     choices: formatterList
-  })
-  .option('no-toc', {
-    describe: 'Disable creating a Table of Contents in the formatters that support them',
+})
+.option('skip-toc', {
+    describe: 'Skips generating the Table of Contents',
     type: 'boolean',
     default: false
-  })
-  .help()
-  .argv;
+})
+.help()
+.argv;
+
+console.log(options);
 
 // Check to see if the given formatter exists
 fs.readdirSync("formatters").forEach(file => {
     if (file.endsWith(".js")) {
         var formatter = require(`./formatters/${file}`);
 
-        if (options.formatter == formatter.formatterId) {
+        if (options.format == formatter.formatterId) {
             console.log(`INFO: Matched formatter with ${formatter.formatterName}`);
             formatterMatch = true;
             selectedFormatter = formatter
@@ -66,7 +68,7 @@ fs.readdirSync("collectors").forEach(async file => {
             console.log(`INFO: Matched url with ${collector.collectorName}`);
             collectorMatch = true;
             var data = await collector.process(options.url);
-            selectedFormatter.format(data);
+            selectedFormatter.format(data, options);
         }
     }
 });
